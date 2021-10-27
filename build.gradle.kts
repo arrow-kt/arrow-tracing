c06b0bb14a39f69e40a9988216ed3976120a13d8
i-walker <46971368+i-walker@users.noreply.github.com>
2021-10-27 12:50:43 +0200

    refine build

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ allprojects {
   apply(plugin = "org.gradle.idea")
 
   group = "io.arrow-kt"
-  version = "0.0.1-SNAPSHOT"
+  version = "0.1.0-SNAPSHOT"
 
   repositories {
     google()
@@ -41,7 +41,7 @@ allprojects {
 }
 
 subprojects {
-  if (!name.startsWith("interpreters")) {
+  if (!listOf("interpreters").contains(name)) {
     apply(plugin = "org.jetbrains.kotlin.multiplatform")
 
     kotlin {
@@ -55,32 +55,6 @@ subprojects {
             }
           }
         }
-
-        js(IR) {
-          browser()
-          nodejs()
-          useCommonJs()
-          binaries.executable()
-        }
-
-        linuxX64()
-
-        mingwX64()
-
-        iosArm32()
-        iosArm64()
-        iosSimulatorArm64()
-        iosX64()
-        macosArm64()
-        macosX64()
-        tvosArm64()
-        tvosSimulatorArm64()
-        tvosX64()
-        watchosArm32()
-        watchosArm64()
-        watchosSimulatorArm64()
-        watchosX64()
-        watchosX86()
       }
 
       sourceSets {
@@ -89,10 +63,6 @@ subprojects {
             implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
             compileOnly("io.arrow-kt:arrow-fx-coroutines:1.0.1-SNAPSHOT")
           }
-        }
-
-        val jsMain by getting {
-          dependsOn(commonMain)
         }
 
         val jvmMain by getting {
@@ -115,48 +85,6 @@ subprojects {
           dependencies {
             implementation("io.kotest:kotest-runner-junit5:5.0.0.M3")
           }
-        }
-
-        val jsTest by getting {
-          dependsOn(commonTest)
-          dependsOn(jsMain)
-        }
-
-        val mingwX64Main by getting
-        val linuxX64Main by getting
-        val iosArm32Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosX64Main by getting
-        val macosArm64Main by getting
-        val macosX64Main by getting
-        val tvosArm64Main by getting
-        val tvosSimulatorArm64Main by getting
-        val tvosX64Main by getting
-        val watchosArm32Main by getting
-        val watchosArm64Main by getting
-        val watchosSimulatorArm64Main by getting
-        val watchosX64Main by getting
-        val watchosX86Main by getting
-
-        create("nativeMain") {
-          dependsOn(commonMain)
-          mingwX64Main.dependsOn(this)
-          linuxX64Main.dependsOn(this)
-          iosArm32Main.dependsOn(this)
-          iosArm64Main.dependsOn(this)
-          iosSimulatorArm64Main.dependsOn(this)
-          iosX64Main.dependsOn(this)
-          macosArm64Main.dependsOn(this)
-          macosX64Main.dependsOn(this)
-          tvosArm64Main.dependsOn(this)
-          tvosSimulatorArm64Main.dependsOn(this)
-          tvosX64Main.dependsOn(this)
-          watchosArm32Main.dependsOn(this)
-          watchosArm64Main.dependsOn(this)
-          watchosSimulatorArm64Main.dependsOn(this)
-          watchosX64Main.dependsOn(this)
-          watchosX86Main.dependsOn(this)
         }
       }
     }
--- interpreters/opensensus/build.gradle.kts
+++ interpreters/opensensus/build.gradle.kts
@@ -10,13 +10,13 @@ kotlin {
         implementation("io.opencensus:opencensus-exporter-trace-ocagent:0.28.3")
       }
     }
-    jsMain {
-      dependencies {
-        //implementation(npm("@opencensus/core", "0.1.0", generateExternals = true))
-        //implementation(npm("@opencensus/nodejs", "0.1.0", generateExternals = true))
-        //implementation(npm("@opencensus/nodejs-base", "0.1.0", generateExternals = true))
-        //implementation(npm("@opencensus/exporter-ocagent", "0.1.0", generateExternals = true))
-      }
-    }
+    //jsMain {
+    // dependencies {
+    //implementation(npm("@opencensus/core", "0.1.0", generateExternals = true))
+    //implementation(npm("@opencensus/nodejs", "0.1.0", generateExternals = true))
+    //implementation(npm("@opencensus/nodejs-base", "0.1.0", generateExternals = true))
+    //implementation(npm("@opencensus/exporter-ocagent", "0.1.0", generateExternals = true))
+    //}
+    //}
   }
 }
